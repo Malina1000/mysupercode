8ab9b1eacff16d47f1ff63a99a3bd9a0b6a45f63
Malina1000 <malinkavecherinka00@gmail.com>
2024-03-19 15:03:22 +0300

    aa

--- 4-3-pwm.py
+++ 4-3-pwm.py
@@ -24,21 +24,21 @@ dc = 0
 GPIO.setmode (GPIO.BCM)
 
 GPIO.setup (24, GPIO.OUT)
-GPIO.setup (dac, GPIO.OUT)
 
 p = GPIO.PWM (24, 1000)
 
-p.start (dc)
+
+p.start (0)
 
 try:
     while True:
-        coeff = float (input("Fill coeff = "))
-        int_c = DecimalToBinary(int(coeff / 100 * 255))
-        GPIO.output(dac, int_c)
+        coeff = int (input ("Fill coeff = "))
+        
         p.ChangeDutyCycle(coeff)
         print(coeff * 3.3 / 100)
 finally:
-    GPIO.output(dac, 0)
+    GPIO.output(24, 0)
     GPIO.cleanup ()
 
+GPIO.output(24, 0)
 GPIO.cleanup ()
